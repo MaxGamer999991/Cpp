02493914ca73f5cd8403b23306f909a4c101690c
Maximilian Elia <125806988+MaxGamer999991@users.noreply.github.com>
2024-07-05 19:47:02 +0200

    Update Test2.cpp

--- Test2.cpp
+++ Test2.cpp
@@ -1,54 +1,45 @@
-#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 int main() {
-  // SDL initialisieren
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    SDL_Quit();
+  // Framebuffer-Datei öffnen
+  int fd = open("/dev/fb0", O_RDWR);
+
+  if (fd < 0) {
+    perror("Fehler beim Öffnen des Framebuffers");
     return 1;
   }
 
-  // Fenster erstellen
-  SDL_Window *window = SDL_CreateWindow("Pixel manipulation", 0, 0, 640, 480, SDL_WINDOW_SHOWN);
-
-  if (window == nullptr) {
-    SDL_Quit();
+  // Framebuffer-Größe abrufen
+  struct fb_var_screeninfo screen_info;
+  if (ioctl(fd, FBIOGET_VSCREENINFO, &screen_info) < 0) {
+    perror("Fehler beim Abrufen der Framebuffer-Informationen");
+    close(fd);
     return 1;
   }
 
-  // Renderer erstellen
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+  // Framebuffer-Speicher in den Speicher mappen
+  void *fbptr = mmap(NULL, screen_info.xsize * screen_info.ysize * screen_info.bits_per_pixel / 8,
+                    PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-  if (renderer == nullptr) {
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+  if (fbptr == MAP_FAILED) {
+    perror("Fehler beim Zuordnen des Framebuffers");
+    close(fd);
     return 1;
   }
 
-  // Pixel mit RGB-Wert ändern
-  int x = 100;
-  int y = 100;
-  Uint8 r = 255;
-  Uint8 g = 0;
-  Uint8 b = 0;
+  // Rotes Pixel zeichnen
+  unsigned char *pixel = (unsigned char *)fbptr + screen_info.xsize * 100 + 100 * screen_info.bits_per_pixel / 8;
+  *pixel = 0xFF; // Rot
+  *(pixel + 1) = 0x00; // Grün
+  *(pixel + 2) = 0x00; // Blau
 
-  SDL_SetRenderDrawColor(renderer, r, g, b);
-  SDL_RenderDrawPoint(renderer, x, y);
+  // Framebuffer-Zuordnung aufheben
+  munmap(fbptr, screen_info.xsize * screen_info.ysize * screen_info.bits_per_pixel / 8);
 
-  // Fenster aktualisieren
-  SDL_RenderPresent(renderer);
-
-  // Wartezeit, bis Benutzer eine Taste drückt
-  SDL_Event event;
-  while (true) {
-    if (SDL_PollEvent(&event) && event.type == SDL_QUIT) {
-      break;
-    }
-  }
-
-  // Ressourcen freigeben
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  // Framebuffer-Datei schließen
+  close(fd);
 
   return 0;
 }
